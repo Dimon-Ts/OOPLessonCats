96e4ae49a2b2772735bd997edfb9cad0ac8c55c3
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-11-02 11:14:38 +0300

    ООП.Урок с Зурабом Белым.Введение и практика.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat murzik = new Cat("Мурзик",8);
+        //murzik.name = "Бегемот";
+        //murzik.age = 5;
+        murzik.meow();
+
+        Cat begemot = new Cat("Бегемот",5);
+        //begemot.name = "Бегемот";
+        //begemot.age = 5;
+        begemot.meow();
+
+
+    }
+}
